ac7e5f4c620c36d0532a3cb79141efd91380f44d
Cybersport <Cybersport@26kadr.internal>
2024-12-04 15:34:06 +0300

    lol

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import Icon from "../src/img/11.svg"
+import arrowleftIcon from "../src/img/ArrowLeft.svg"
+import arrowrightIcon from "../src/img/ArrowRight.svg"
+import dlIcon from "../src/img/BI.svg"
+import button1Icon from "../src/img/Button1.svg"
+import buttonIcon from "../src/img/Button.svg"
+import dotsIcon from "../src/img/Dots.svg"
+import fbIcon from "../src/img/FB.svg"
+import frame48Icon from "../src/img/Frame48.svg"
+import frame49Icon from "../src/img/Frame49.svg"
+import frame50Icon from "../src/img/Frame50.svg"
+import frame51Icon from "../src/img/Frame51.svg"
+import frame72Icon from "../src/img/Frame72.svg"
+import hbIcon from "../src/img/HB.svg"
+import image2Icon from "../src/img/image2.svg"
+import image3Icon from "../src/img/image3.svg"
+import kfIcon from "../src/img/KF.svg"
+import model111Icon from "../src/img/model111.svg"
+import model11Icon from "../src/img/model11.svg"
+import numeroIcon from "../src/img/Numero.svg"
+import screenshotIcon from "../src/img/Screenshot.svg"
+import shirt1Icon from "../src/img/shirt1.svg"
+import subtractIcon from "../src/img/Subtract.svg"
+import untitled21 from "../src/img/Untitled21.svg"
+import untitled41 from "../src/img/Untitled41.svg"
+import dots1Icon from "../src/img/Dots1.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={dotsIcon}></img>
+        <strong>SUMMER SALE UP TO 50% OFF</strong>
+        <img src={dots1Icon}></img>
+      </header>
+      <div className="h1">
+          <strong style={{ fontSize: '150px' }}>FIRST</strong>
+          <img src={subtractIcon}></img>
+          <strong style={{ fontSize: '150px' }}>LIHGT</strong>
+      </div>
+      <div className="h2">
+        <img src={screenshotIcon}></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
